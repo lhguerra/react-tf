1c092ea00fd93328d51b41942e88377134dbe926
Luiz Henrique Guerra <lhguerra@gmail.com>
2019-04-28 16:13:57 -0300

    style: Fixed code styling.

--- .storybook/config.js
+++ .storybook/config.js
@@ -3,7 +3,6 @@ import { configure } from "@storybook/react";
 // automatically import all files ending in *.stories.tsx
 const req = require.context("../components", true, /\.stories\.tsx$/);
 
-console.log(req);
 function loadStories() {
   req.keys().forEach(filename => req(filename));
 }
