fd4e37ce274b2c5f9fd950f872a02b199a378b17
Luiz Henrique Guerra <lhguerra@gmail.com>
2019-04-28 17:58:09 -0300

    chore: Updated storybook config.

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,7 +1,7 @@
 import { configure } from "@storybook/react";
 
 // automatically import all files ending in *.stories.tsx
-const req = require.context("../components", true, /\.stories\.tsx$/);
+const req = require.context("../src/components", true, /\.stories\.tsx$/);
 
 function loadStories() {
   req.keys().forEach(filename => req(filename));
--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -10,6 +10,8 @@ module.exports = ({ config }) => {
       }
     ]
   });
+
   config.resolve.extensions.push(".ts", ".tsx");
+
   return config;
 };
