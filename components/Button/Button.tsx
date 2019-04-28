61c4941ff21c869cb36047144981eb6aebdf242f
Luiz Henrique Guerra <lhguerra@gmail.com>
2019-04-28 16:14:27 -0300

    feat: Add more styles and props interface to button.

--- components/Button/Button.tsx
+++ components/Button/Button.tsx
@@ -2,9 +2,21 @@ import * as React from "react";
 import styled from "styled-components";
 
 const StyledButton = styled.button`
-  background-color: gray;
+  background-color: #766b5e;
+  border-radius: 5px;
+  color: #ece3cb;
+  border: none;
+  padding: 1rem;
+  font-size: 20px;
+  font-weight: bold;
 `;
 
-const Button = () => <StyledButton>Click Me</StyledButton>;
+interface Props {
+  icon?: React.ReactNode;
+}
+
+const Button = ({ icon }: Props) => (
+  <StyledButton>{icon && icon}Click Me</StyledButton>
+);
 
 export default Button;
