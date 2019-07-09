2a7cb6b965cb6d4c9bfa80539bbd120a6567e679
Luiz Henrique Guerra <lhguerra@gmail.com>
2019-07-08 23:09:27 -0300

    feat: Added dynamic implementation of NewsTeaser component.

--- src/components/NewsTeaser/NewsTeaser.stories.tsx
+++ src/components/NewsTeaser/NewsTeaser.stories.tsx
@@ -2,4 +2,17 @@ import * as React from "react";
 import { storiesOf } from "@storybook/react";
 import NewsTeaser from "./NewsTeaser";
 
-storiesOf("News Teaser", module).add("Default", () => <NewsTeaser />);
+const props = {
+  title: "Checked out the blog?",
+  date: "Wednesday Jan 23",
+  image: {
+    src: "http://lorempixel.com/500/500",
+    alt: "Nice Image"
+  },
+  text:
+    "If you haven't read the official TF2 blog, it's full of insights into our development process, links to notable community productions, and random stories about our lov of hats.Hit the button below to take a look."
+};
+
+storiesOf("News Teaser", module).add("Default", () => (
+  <NewsTeaser {...props} />
+));
--- src/components/NewsTeaser/NewsTeaser.tsx
+++ src/components/NewsTeaser/NewsTeaser.tsx
@@ -1,20 +1,27 @@
 import * as React from "react";
 
-const NewsTeaser = () => (
+interface Image {
+  src: string;
+  alt: string;
+}
+
+interface Props {
+  title: String;
+  date: String;
+  image: Image;
+  text: String;
+}
+
+const NewsTeaser = ({ title, date, image: { src, alt }, text }: Props) => (
   <article>
     <header>
       <h3>News</h3>
     </header>
     <main>
-      <h4>Checked out the blog?</h4>
-      <span>Wednesday Jan 23</span>
-      <img src="http://lorempixel.com/500/500" />
-      <p>
-        If you haven't read the official TF2 blog, it's full of insights into
-        our development process, links to notable community productions, and
-        random stories about our lov of hats. Hit the button below to take a
-        look.
-      </p>
+      <h4>{title}</h4>
+      <span>{date}</span>
+      <img src={src} alt={alt} />
+      <p>{text}</p>
     </main>
     <a>Read More</a>
   </article>
