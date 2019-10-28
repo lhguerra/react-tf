de4a45cbbb29b62268d809bdab222592ad900622
Luiz Henrique Guerra <lhguerra@gmail.com>
2019-10-27 23:03:06 -0300

    chore: Added new color to theme.

--- src/theme.ts
+++ src/theme.ts
@@ -8,7 +8,8 @@ const theme = {
     merlin: "#3a3530",
     copper: "#b86c37",
     pearl: "#FBF2D5",
-    silk: "#B9B0A7"
+    silk: "#B9B0A7",
+    dune: "#2b2724",
   },
   fonts: {
     title: "TF2Font"
