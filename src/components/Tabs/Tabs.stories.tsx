622e12b5b5c6e72b4ce8d3811becb014027e228d
Luiz Henrique Guerra <lhguerra@gmail.com>
2019-12-02 01:10:14 -0300

    feat: Add tabs component

---
+++ src/components/Tabs/Tabs.stories.tsx
@@ -0,0 +1,6 @@
+import * as React from "react";
+import { storiesOf } from "@storybook/react";
+import Tabs from "./";
+
+storiesOf("Tabs", module)
+  .add("Tabs", () => <Tabs />)
---
+++ src/components/Tabs/index.tsx
@@ -0,0 +1,5 @@
+import * as React from 'react'
+
+const Tabs = () => <div>tab</div>
+
+export default Tabs
